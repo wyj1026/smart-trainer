c9c946a77fd88dc6d7e3976d5ba1c613f9d57cc9
wangyijie <wangyijieim@outlook.com>
2018-08-10 20:50:21 +0800

    特征提取基础代码, 未测试

---
+++ ai/features/features.py
@@ -0,0 +1,2 @@
+squat_features = ["stance_shoulder_width", "stance_straightness", "feet", "bend_hips_knees", "back_straight", "head_aligned_back", "depth", "back_hip_angle"]
+pushup_features = []
---
+++ ai/features/squat_features_extractor.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+from features import squat_features
+
+
+def extract_all(squats):
+    return [extract_features_from_squat(squat) for squat in squats]
+
+
+def extract_features_from_squat(squat, features=squat_features):
+    extracted_features = []
+    features_funcs_dict = {}
+    function_name = "extract_{}"
+    for feature_name in features:
+        features_funcs_dict[feature_name] = globals().get(function_name.format(feature_name))
+
+    for feature_name in features:
+        func = features_funcs_dict[feature_name]
+        extracted_features.append(func())
+    return extracted_features
+
+
+def extract_stance_shoulder_width():
+    pass
+
+
+def extract_stance_straightness():
+    pass
+    
+
+def extract_feet():
+    pass
+
+
+def extract_bend_hips_knees():
+    pass
+
+
+def extract_back_straight():
+    pass
+
+
+def extract_head_aligned_back():
+    pass
+
+
+def extract_depth():
+    pass
+
+
+def extract_back_hip_angle():
+    pass
+
+
+def extract_():
+    pass
